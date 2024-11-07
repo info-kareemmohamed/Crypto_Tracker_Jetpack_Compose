4dd3ef24605d994c4a0a43f988193d036c13ec1f
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-11-07 12:12:22 +0200

    Handle network in data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,13 +24,21 @@ android {
     }
 
     buildTypes {
+       debug {
+           buildConfigField("String", "BASE_URL", "\"https://api.coincap.io/v2/\"")
+       }
         release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "BASE_URL", "\"https://api.coincap.io/v2/\"")
+
         }
+
+
+
     }
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
